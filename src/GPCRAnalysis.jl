76cb349533e708b59f06b9a9bff5a7ff36f0ad24
Tim Holy <tim.holy@gmail.com>
2024-05-14 11:42:02 -0500

    Add `align_to_membrane` (#13)
    Add a function to align a GPCR so that the membrane is in the XY plane.
    
    Additional housekeeping:
    - Move AlphaFold functions to their own file.
    - Use consistent naming for alphafold *files* vs *filenames*.
    
    The second point means that `alphafoldfile` is changing its meaning (and arguments).
    It was not exported, so technically we may not need a breaking change, but some
    scripts seem to use it so this also bumps the version number to 0.2.0.

--- src/GPCRAnalysis.jl
+++ src/GPCRAnalysis.jl
@@ -14,6 +14,7 @@ using Distances
 using Hungarian
 using ProgressMeter
 using StaticArrays
+using Rotations
 using TravelingSalesmanHeuristics
 
 using Interpolations
@@ -30,7 +31,8 @@ export @res_str
 
 export SequenceMapping, AccessionCode, MSACode
 export species, uniprotX, query_uniprot_accession
-export try_download_alphafold, query_alphafold_latest, download_alphafolds, alphafoldfiles, getchain, findall_subseq, align_to_axes
+export try_download_alphafold, query_alphafold_latest, download_alphafolds, alphafoldfile, alphafoldfiles, getchain, findall_subseq
+export align_to_axes, align_to_membrane
 export filter_species!, filter_long!, sortperm_msa, chimerax_script
 export project_sequences, columnwise_entropy, align, residue_centroid, residue_centroid_matrix, alphacarbon_coordinates, alphacarbon_coordinates_matrix, mapclosest, chargelocations, positive_locations, negative_locations
 export StructAlign, residueindex, ismapped
@@ -42,8 +44,10 @@ export features_from_structure
 include("naming_conventions.jl")
 include("utils.jl")
 include("msa.jl")
+include("alphafold.jl")
 include("analyze.jl")
 include("tmalign.jl")
+include("align.jl")
 include("ballesteros_weinstein.jl")
 include("properties.jl")
 include("chimerax.jl")
